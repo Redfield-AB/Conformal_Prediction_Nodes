24d2da7bcc3fa3482a5caca91f8de5457d1cbfe8
Gabriel Einsdorf <gabriel.einsdorf@knime.com>
2020-03-02 18:26:27 +0100

    Add testcase collector to the testing plugin

---
+++ se.redfield.cp.testing/src/se/redfield/cp/testing/TestcaseCollector.java
@@ -0,0 +1,13 @@
+package se.redfield.cp.testing;
+
+import org.knime.testing.core.AbstractTestcaseCollector;
+
+/**
+ * This class is required for the unit tests to be collected.
+ */
+public class TestcaseCollector extends AbstractTestcaseCollector {
+
+	public TestcaseCollector() {
+	}
+
+}
